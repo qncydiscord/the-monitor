f14d9de37ffee787ccdefb340cf3d5f0786c5c81
qncydiscord <50281422+qncydiscord@users.noreply.github.com>
2019-05-05 04:33:55 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+// Defining Variables and constants.
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const PREFIX = '!';
+
+var version = '1.0.0';
+
+bot.on('ready', () =>{
+    console.log('This bot is a GO!');
+})
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.sendMessage('pong!');
+        break;
+        case 'version':
+            message.channel.sendMessage('Version ' + version);
+        break;
+        case 'clear':
+            if(!args[1]) return message.reply('Please define how many messages you want to delete.')
+            message.channel.bulkDelete(args[1])
+        break;
+    }
+})
+
+bot.login(process.env.BOT_TOKEN);
