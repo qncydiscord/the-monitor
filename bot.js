08132cc283bda47d5f02c3884493ad84d7b5d461
qncymac <50281422+qncydiscord@users.noreply.github.com>
2020-01-19 04:18:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ const bot = new Discord.Client();
 
 const PREFIX = '!';
 
-var information = '1.0.5';
+var information = '1.1';
+//functions
 
 bot.on('ready', () =>{
     console.log('This bot is a GO!');
@@ -24,20 +25,45 @@ bot.on('message', message=>{
 
     let args = message.content.substring(PREFIX.length).split(" ");
 
-    switch(args[0]){
+    switch (args[0]) {
         case 'ping':
             message.channel.sendMessage('pong!');
-        break;
+            break;
         case 'information':
             message.channel.sendMessage('Made by Quincy#4663, Version ' + information);
-        break;
+            break;
         case 'clear':
-            if(!args[1]) return message.reply('Please define how many messages you want to delete.')
+            if (!args[1]) return message.reply('Please define how many messages you want to delete.')
             message.channel.bulkDelete(args[1])
-        break;
+            break;
         case 'avatar':
             message.reply(message.author.avatarURL);
+            break;
+        case 'poll':
+            const Embed = new RichEmbed()
+                .setColor(0xFF0000)
+                .setTitle("Initiate Poll")
+                .setDescription("!poll Choice 1 Choice 2 (Leave a space in the middle.) Yes or No poll only.")
+            if (!args[1]) {
+                message.channel.send(Embed);
+                break;
+            }
+
+            let msgArgs = args.slice(1).join(" ");
+
+            message.channel.send("🗒️" + "**" + msgArgs + "**").then(messageReaction => {
+                messageReaction.react("👍");
+                messageReaction.react("👎");
+                message.delete(1000).catch(console.error);
+            });
         break;
+        case 'help':
+            message.author.sendMessage('Prefix is: "!", following commands are currently avaliable for use: information, help, poll, ping.')
+        break;
+        case 'changelog':
+            message.channel.sendMessage('Poll added, few things fixed serverside.')
+        break;
+
     }
 })
 
